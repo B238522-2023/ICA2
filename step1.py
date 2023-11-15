b03b9877d48a43dd0bfb03ed7b3a95c5e8bc00f1
Ruoyu Chen <s2552113@bioinfmsc5.ed.ac.uk>
2023-11-15 23:12:23 +0000

    output the fasta format sequence into a file which named by the user

--- step1.py
+++ step1.py
@@ -1,67 +1,73 @@
 #!/usr/bin/python3
 #Importing the subprocess module to execute external commands
 import subprocess
-import argparse
+
 #Defining the function fasta_ncbi to execute the NCBI query and get the output in fasta format
 def efetch_fasta_ncbi(protein_name, organism):
-	print("Starting query from NCBI")
-	try:
-		#build a string called query to contain protein name and organism
-		query = f"{protein_name} AND {organism}[ORGN]"
-		#define the command of searching in NCBI as cmd
-		cmd = f"esearch -db protein -query '{query}' | efetch -format fasta"
-		#using subprocess run the command and capture its output and errors
-		result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-
-		#check if the command excuted successfully,if not then raise an exception
-		if result.returncode != 0:
-			raise Exception(result.stderr)
-
-		print("Finished query from NCBI")
-		#return the standard output in fasta format
-		return result.stdout
-	except Exception as e:
+	print("Starting querying from NCBI")
+	#build a string called query to contain protein name and organism
+	query = f"{protein_name} AND {organism}[ORGN]"
+	#define the command of searching in NCBI as cmd
+	cmd = f"esearch -db protein -query '{query}' | efetch -format fasta"
+	#using subprocess run the command and capture its output and errors
+	result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+	#check if the command excuted successfully,if not then raise an exception
+	if result.returncode != 0:
 		print("An error occurred in efetch_fasta_ncbi function")
-		#If any error occurs, throw an exception
-		raise
+		return None
+	#tell the user their query is finish
+	print("Finished querying from NCBI")
+	#return the standard output in fasta format
+	return result.stdout
 
-#define if the user's input is empty or not
-def validate_input(input_str, input_type):
-	#if the input is empty,then tell the user their input is empty and modify it
-	if not input_str.strip():
-		raise ValueError(f"{input_type} cannot be empty. Please enter a valid {input_type.lower()}.")
+#count the sequence nuber of the output and return it to 'fasta_data'
+def count_sequences_in_fasta(fasta_data):
+	return fasta_data.count('>')
 
+#show the user's input on the screen to check
+#define a function called get_user_input that takes two arguments 
+def get_user_input(input_prompt, input_type):
+	#create an infinite loop,execute code until encounter a return statement
+	while True:
+		user_input = input(input_prompt)
+		#check if the user's input is valid,using strip function to remove whitespace,if result is empty,means the user's input has no valid content
+		if not user_input.strip():
+			print(f"{input_type} cannot be empty. Please enter a valid {input_type}.")
+			continue
+		print(f"Your input is: '{user_input}'")
+		if input("Do you confirm yout input? (yes/no): ").lower() == "yes":
+			return user_input
 
+#save the output fasta into a file
+def save_output_to_file(data, filename):
+  #open the file in a write mode
+	with open(filename, 'w') as file:
+  #write the data into a file
+		file.write(data)
+	print(f"Data saved to {filename}")
 
 while True:
-	try:
-		#ask the user to input protein name
-		protein_name = input("Enter the protein name:")
-		#check if the protein name is valid
-		validate_input(protein_name, "Protein name")
-		#ask the user to input the organism name
-		organism = input("Enter the organism name: ")
-		#check if it is valid
-		validate_input(organism, "Organism name")
-
-		# Executing the query based on user input
-		fasta_result = efetch_fasta_ncbi(protein_name, organism)
-		#if not find in NCBI, raise the error
-		if not fasta_result:
-			raise ValueError("No results found for the given protein name and organism. Please try again.")
+	#ask the user to input protein name
+	protein_name = get_user_input("Enter the protein name:", "protein name")
+	#ask the user to input the organism name
+	organism = get_user_input("Enter the organism name: ", "organism name")
+	# Executing the query based on user input
+	fasta_result = efetch_fasta_ncbi(protein_name, organism)
 
 	# Printing the query results
-		print(fasta_result)
-		break  # if successfully queried exit the loop
-
-	# Capture and print input errors of the user
-	except Exception as e:
-		# capture and print errors while obtaining data from NCBI
-		print(f"An error occurred while fetching data: {e}")
-
-		# ask the user if they want to try again or exit.
-		retry = input("Do you want to try again? (yes/no): ").lower()
-		if retry != "yes":
+	if fasta_result:
+    #count the number of sequences queried
+		sequence_count = count_sequences_in_fasta(fasta_result)
+		print(f"Number of sequences found: {sequence_count}")
+    #ask the user to put the output into a file and let them to name the filename
+		if input("Do you want to save the results to a file? (yes/no): ").lower() == "yes":
+			filename = get_user_input("Enter the filename to save: ", "Filename")
+			save_output_to_file(fasta_result, filename)
+    #ask if the user want to query other proteins and organisms
+    #if they do not want to query other sequences, then finish the query
+		if input("Do you want to continue querying other sequences? (yes/no): ").lower() != "yes":
 			break
-
-
+	else:
+		print("No results found for the given protein name and organism. Please try again.")
+		if input("Do you want to try again? (yes/no): ").lower() != "yes":
+			break

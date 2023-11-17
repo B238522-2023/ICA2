1d42b117f903b957c28f8e1426bc766daa822ad3
Ruoyu Chen <s2552113@bioinfmsc5.ed.ac.uk>
2023-11-17 19:17:33 +0000

    add error trap to step1

--- step1.py
+++ step1.py
@@ -3,41 +3,48 @@
 import subprocess
 
 #Defining the function fasta_ncbi to execute the NCBI query and get the output in fasta format
+#build a string called query to contain protein name and taxon_group
+#define the command of searching in NCBI as cmd
+#using subprocess run the command and capture its output and errors
 def efetch_fasta_ncbi(protein_name, taxon_group):
     print("Starting querying from NCBI")
-    #build a string called query to contain protein name and taxon_group
-    query = f"{protein_name}[Protein] AND {taxon_group}[Organism] NOT PARTIAL"
-    #define the command of searching in NCBI as cmd
+    query = f"{protein_name}[Protein] AND {taxon_group}[Organism] NOT PARTIAL" 
     cmd = f"esearch -db protein -query '{query}' | efetch -format fasta"
-    #using subprocess run the command and capture its output and errors
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     #check if the command excuted successfully,if not then raise an exception
     if result.returncode != 0:
         print("An error occurred in efetch_fasta_ncbi function")
         return None
-    #tell the user their query is finish
-    print("Finished querying from NCBI")
-    #return the standard output in fasta format
-    return result.stdout
+    
+    print("Finished querying from NCBI")#tell the user their query is finish
+    return result.stdout # return the standard output in fasta format
 
 #count the sequence nuber of the output and return it to 'fasta_data'
 def count_sequences_in_fasta(fasta_data):
     return fasta_data.count('>')
 
-#show the user's input on the screen to check
-#define a function called get_user_input that takes two arguments 
-def get_user_input(input_prompt, input_type):
-    #create an infinite loop,execute code until encounter a return statement
+#show the user's input on the screen to confirm
+
+def get_user_inputs():#create an infinite loop,execute code until encounter a return statement
     while True:
-        user_input = input(input_prompt)
-        #check if the user's input is valid,using strip function to remove whitespace,if result is empty,means the user's input has no valid content
-        if not user_input.strip():#if the user's input is not empty,nothing happeneds here
-            print(f"{input_type} cannot be empty. Please enter a valid {input_type}.")
-            continue 
-        print(f"Your input is: '{user_input}'") #let user confirm the input
-    #if user confirmed the input, using it for the query
-        if input("Do you confirm yout input? (y/n): ").lower() == "y":
-            return user_input
+        protein_name = input("Enter the protein name: ").strip()
+        if not protein_name:
+            print("Protein name annot be empty.Please enter a valid protein name.")
+            continue
+
+        taxon_group = input("Enter the taxon group: ").strip()
+        if not taxon_group:
+            print("Taxon group cannot be empty. Please enter a valid taxon group.")
+            continue
+
+        print(f"Your protein name is: '{protein_name}'")
+        print(f"Your taxon group is: '{taxon_group}'")
+
+        if input("Do you confirm your inputs? (y/n): ").lower() == "y":
+            return protein_name, taxon_group
+        else:
+            print("Please re-enter your inputs.")
+
 
 #ask the user to name the filename
 def get_filename_input(input_prompt):
@@ -61,31 +68,30 @@ def save_output_to_file(data, filename):
         file.write(data)
     print(f"Data saved to {filename}")
 
-while True:
-    #ask the user to input protein name
-    protein_name = get_user_input("Enter the protein name:", "protein name")
-    #ask the user to input the taxon_group name
-    taxon_group = get_user_input("Enter the taxon_group name: ", "taxon_group name")
-    # Executing the query based on user input
-    fasta_result = efetch_fasta_ncbi(protein_name, taxon_group)
-
-    # Printing the query results
-    if fasta_result:
-    #count the number of sequences queried
-        sequence_count = count_sequences_in_fasta(fasta_result)
-        print(f"Number of sequences found: {sequence_count}")
-    #ask the user to put the output into a file and let them to name the filename
-    
-        if input("Do you want to save the results to a file? (y/n): ").lower() == "y":
-            filename = get_filename_input("Enter the filename to save (it will be saved with .fasta extension): ")
-            save_output_to_file(fasta_result, filename)
+#define a main function 
+#define the protein and taxon group according to the user's input
+def main():
+    try:#add an error trapping to check the code
+        while True:
+          protein_name,taxon_group = get_user_inputs() 
+          fasta_data = efetch_fasta_ncbi(protein_name, taxon_group)
       
-   
-    #ask if the user want to query other proteins and organisms
-    #if they do not want to query other sequences, then finish the query
-        if input("Do you want to continue querying other sequences? (y/n): ").lower() != "y":
-            break
-    else:#if the sequence number is 0,means find nothing from NCBI,using 'print'to tell the user
-        print("No results found for the given protein name and organism. Please try again.")
-        if input("Do you want to try again? (y/n): ").lower() != "y":
-            break
+          if fasta_data:
+              sequence_count = count_sequences_in_fasta(fasta_data)#count the number of fasta data
+              print(f"Number of sequence found: {sequence_count}")
+        
+              if input("Do you want to save the results to a file? (y/n): ").lower() == "y":
+                  filename = get_filename_input("Enter the filename to save (it will be saved with .fasta extension): ")
+                  save_output_to_file(fasta_data, filename)
+              if input("Do you want to continue querying other sequences? (y/n): ").lower() != "y":
+                  break
+          else:
+              print("No results found for the given protein name and organism. Please try again.")
+              if input("Do you want to try again? (y/n): ").lower() != "y":
+                  break
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+#check whether the script is run as a master program,if is ,the variable '__name__' will be set to main and main function will be called, allowing other scripts to use the functions defined in this script, without executing the code in the main function.
+if __name__ == "__main__":
+    main()
